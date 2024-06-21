ea8ac0d609937b0be792229a7946f360e84431c5
gabriellama <gabriellamalvares@gmail.com>
2024-06-21 13:39:53 -0300

    home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import homeLogo from '../../assets/GDlPyXuXoAALvh8.png'
+
+const Home = () => {
+  return (
+    <>
+        <h1 className= 'titulo'>Home</h1>
+
+        <img src={homeLogo} className='img' alt="Deus cuida de mim" />
+    </>
+    
+  )
+}
+
+export default Home
